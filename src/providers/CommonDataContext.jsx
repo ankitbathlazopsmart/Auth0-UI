b87684715eb35b97077f0b8650a219730a6ad5d6
ankit Bathla <ankit.bathla@zopsmart.com>
2021-10-20 19:30:42 +0530

    feat: update

--- src/providers/CommonDataContext.jsx
+++ src/providers/CommonDataContext.jsx
@@ -1,21 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const CommonDataContext = React.createContext({});
 
 const CommonDataProvider = (props) => {
+    const [connections, setConn] = useState([]);
     useEffect(() => {
         const getCommonData = async () => {
             const res = await axios.get(
                 "/client/9dR6Ug2BQQbmBMp6grHf1R962NBigRxg.js?t1634713429698"
             );
-            console.log(JSON.parse(res.data.slice(16, -2)));
+            const data = JSON.parse(res.data.slice(16, -2));
+            console.log(data.strategies);
+            setConn(data.strategies);
         };
 
         getCommonData();
     }, []);
     return (
-        <CommonDataContext.Provider value={{}}>
+        <CommonDataContext.Provider value={{ connections }}>
             {props.children}
         </CommonDataContext.Provider>
     );
